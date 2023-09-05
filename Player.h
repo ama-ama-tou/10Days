4308c4731cb1f78b02f02ab6c2929bcf4dbe56cc
陽人 加瀬 <0te06559726481t@au.com>
2023-09-05 21:55:11 +0900

    a

--- Player.h
+++ Player.h
@@ -16,18 +16,8 @@ class Player :
 	//===============
 	
 	void Init(Vec2 pos, Vec2 size, Vec2 localCoOrigin, int GH, Vec2 imageLtPos, Vec2 imageSize) {
-		vertex_[0] = pos;
-		vertex_[1] = Vec2(pos.x + size.x, pos.y);
-		vertex_[2] = Vec2(pos.x, pos.y + size.y);
-		vertex_[3] = Vec2(pos.x + size.x, pos.y + size.y);
-
+		Quad::Init(pos, size, localCoOrigin, GH, imageLtPos, imageSize);
 		speed = 8.0f;
-
-		localCo.setOrigin(localCoOrigin);
-
-		GH_ = GH;
-		imageLtPos_ = imageLtPos;
-		imageSize_ = imageSize;
 	}
 
 	void Move(char* keys,char* preKeys);
