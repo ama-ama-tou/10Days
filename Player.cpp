a02cf9a2306a535e00140fff987af0975e3f3bd9
tou ama <ira341394@gmail.com>
2023-09-05 22:22:04 +0900

    a

--- Player.cpp
+++ Player.cpp
@@ -1,14 +1,16 @@
 ﻿#include "Player.h"
 
 void Player::Move(char* keys,char* preKeys) {
-	//���E�̈ړ�
+	//左右の移動
+	
 	if (keys[DIK_A] && !preKeys[DIK_A]) {
 		pos.x += speed;
 	} else if (keys[DIK_D] && !preKeys[DIK_D]) {
 		pos.x -= speed;
 	}
 
-	//�㉺�̈ړ�
+	//上下の移動
+	
 	if (keys[DIK_W] && !preKeys[DIK_W]) {
 		pos.y -= speed;
 	} else if (keys[DIK_S] && !preKeys[DIK_S]) {
