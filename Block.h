d2cf4d4dfaaa750eeee508bb684a08876da0e520
tou ama <ira341394@gmail.com>
2023-09-06 09:56:59 +0900

    blockType

--- Block.h
+++ Block.h
@@ -1,12 +1,18 @@
 ﻿#pragma once
 #include "Quad.h"
-
+enum BlockType {
+    NON,
+    N_POLE,//N極
+    S_POLE,//S極
+    WALL//壁
+};
 
 class Block :
     public Quad {
 
 
-
+    bool isLocated;
+    int 
 
 
 };
