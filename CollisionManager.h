d4d5b3c97084531175caffe2c6c0825716d2ebf5
tou ama <ira341394@gmail.com>
2023-09-06 14:19:38 +0900

    update

--- CollisionManager.h
+++ CollisionManager.h
@@ -1,4 +1,19 @@
 #pragma once
+#include"Player.h"
+#include"Block.h"
+#include<vector>
+
 class CollisionManager {
+	Player player_;
+	std::vector<Block*> block_;
+public:
+	CollisionManager(Player& player) :player_(player) {};
+
+	void AddBlock(Block* block) {
+		block_.push_back(block);
+	}
+	void Collision();
+
+
 };
 
