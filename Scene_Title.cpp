c7f0c98ba062dbd5ae6d2411e05c37ca2a54e23f
tou ama <ira341394@gmail.com>
2023-09-06 17:20:18 +0900

    s

--- Scene_Title.cpp
+++ Scene_Title.cpp
@@ -50,7 +50,12 @@ void Scene_Title::Load() {
 
 
 void Scene_Title::Update() {
-
+	if (Button_goManu.getIsClicked()) {
+		sceneNum = SCENE_MENU;
+	}
+	if (Button_goSelect.getIsClicked()) {
+		sc
+	}
 
 
 }
