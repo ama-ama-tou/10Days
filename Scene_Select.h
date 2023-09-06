9b7b6b2006548a8b559c4485a128b3195e9b03e1
陽人 加瀬 <0te06559726481t@au.com>
2023-09-06 15:35:19 +0900

    ステージセレクトクラス追加

--- Scene_Select.h
+++ Scene_Select.h
@@ -1,9 +1,24 @@
 #pragma once
 #include "Scene.h"
+#include "Button.h"
 
 class Scene_Select:
 	public Scene {
 
+	
+private:
+
+	//======================
+	//メンバ変数
+	//======================
+
+	Button stage_[9];
+	
+public:
+	//=======================
+	//ゲッター
+	//=======================
+	int getSelectedStage(Vec2 mousePos, int clickState);
 
 };
 
