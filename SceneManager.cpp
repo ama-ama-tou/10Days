81a8a9d6c146178b8dd390187557c47b1d7f521c
tou ama <ira341394@gmail.com>
2023-09-07 16:08:41 +0900

    sceneManagerの追加

--- SceneManager.cpp
+++ SceneManager.cpp
@@ -1,1 +1,29 @@
 #include "SceneManager.h"
+
+SceneManager::SceneManager() {
+	//各シーンの配列
+	sceneArr_[SCENE_TITLE] = std::make_unique<Scene_Title>();
+	sceneArr_[SCENE_SELLECT] = std::make_unique<Scene_Select>();
+	sceneArr_[SCENE_GAME] = std::make_unique<Scene_Game>();
+
+	//初期シーン
+	sceneNum_ = SCENE_TITLE;
+}
+
+void SceneManager::Update() {
+	preSceneNum_ = sceneNum_;
+
+	if (sceneNum_!=preSceneNum_) {
+		//シーンが変わったら初期化
+		sceneArr_[sceneNum_]->Load();
+	}
+
+	//現在のシーンの更新処理
+	sceneArr_[sceneNum_]->Update();
+
+	//現在のシーンの描画処理
+	sceneArr_[sceneNum_]->Draw();
+
+
+}
+
