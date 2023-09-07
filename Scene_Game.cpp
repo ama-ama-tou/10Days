b88afc4e028ee1ea9aa877eeee1eeb8ba29a5b42
tou ama <ira341394@gmail.com>
2023-09-07 14:07:02 +0900

    a

--- Scene_Game.cpp
+++ Scene_Game.cpp
@@ -1,1 +1,33 @@
 #include "Scene_Game.h"
+
+Scene_Game::Scene_Game() {
+	stageArr_[STAGE_1ST] = std::make_unique<Stage>("./Resources/csv/stage1.CSV");
+	stageArr_[STAGE_2ND] = std::make_unique<Stage>("./Resources/csv/stage2.CSV");
+	stageArr_[STAGE_3RD] = std::make_unique<Stage>("./Resources/csv/stage3.CSV");
+	stageArr_[STAGE_4TH] = std::make_unique<Stage>("./Resources/csv/stage4.CSV");
+	stageArr_[STAGE_5TH] = std::make_unique<Stage>("./Resources/csv/stage5.CSV");
+	stageArr_[STAGE_6TH] = std::make_unique<Stage>("./Resources/csv/stage6.CSV");
+	stageArr_[STAGE_7TH] = std::make_unique<Stage>("./Resources/csv/stage7.CSV");
+	stageArr_[STAGE_8TH] = std::make_unique<Stage>("./Resources/csv/stage8.CSV");
+	stageArr_[STAGE_9TH] = std::make_unique<Stage>("./Resources/csv/stage9.CSV");
+	stageArr_[STAGE_10TH] = std::make_unique<Stage>("./Resources/csv/stage10.CSV");
+	stageArr_[STAGE_11TH] = std::make_unique<Stage>("./Resources/csv/stage11.CSV");
+	stageArr_[STAGE_12TH] = std::make_unique<Stage>("./Resources/csv/stage12.CSV");
+
+	stageNum = STAGE_1ST;
+
+}
+
+void Scene_Game::Load() {
+		stageArr_[stageNum]->Init();
+}
+
+void Scene_Game::Update() {
+	stageArr_[stageNum]->Update();
+}
+
+void Scene_Game::Draw() {
+	stageArr_[stageNum]->Draw();
+}
+
+void Scene_Game::Unload() {};
--- Scene_Game.h
+++ Scene_Game.h
@@ -1,8 +1,26 @@
 #pragma once
 #include "Scene.h"
+#include"Stage.h"
+
 
 class Scene_Game:
 	public Scene {
+
+	int stageNum;
+	int preStageNum;
+
+	std::unique_ptr<Stage> stageArr_[12];
+
+public:
+	Scene_Game();
+
+	void Load()override;
+	void Update()override;
+	void Draw()override;
+	void Unload()override;
+
+	StageNum setPlayStage(StageNum selectedStage){}
+
 
 };
 
