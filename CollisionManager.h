9d54ae0e52029f34bdd0c5e4d3aae3fac9faadf0
陽人 加瀬 <0te06559726481t@au.com>
2023-09-07 20:51:23 +0900

    ステージセレクトボタンの位置の値の調節

--- CollisionManager.h
+++ CollisionManager.h
@@ -7,14 +7,14 @@ class CollisionManager {
 	Player player_;
 	Player* playerPos_;
 	Block*** block_;
-//	Block blockType_;
+	Block blockType_;
 
 	//各頂点の番地
 	int rowAddress_[4] = { 0 };
 	int colAddress_[4] = { 0 };
 
 public:
-//	CollisionManager(Player& player,Block***& block , Block*& blockType) :player_(player),block_(block),blockType_(blockType) {};
+	CollisionManager(Player& player,Block***& block , Block*& blockType) :player_(player),block_(block),blockType_(blockType) {};
 
 	//当たり判定
 	void playerCollision();
