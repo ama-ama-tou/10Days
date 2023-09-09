e73210b95e12388889bf451c40648dffc8782403
aotan119 <abasuknba.allj@icloud.com>
2023-09-09 23:34:38 +0900

    ファイル名の修正

--- Scene_Title.cpp
+++ Scene_Title.cpp
@@ -6,7 +6,7 @@ void Scene_Title::Load() {
 	Vec2 titlePos;
 	titlePos=Vec2{ 327.0f, 200.0f };
 	Vec2 titleSize{ 625.0f,125.0f };
-	int titleGH = Novice::LoadTexture("./Resource/image/obj/title.png");
+	int titleGH = Novice::LoadTexture("./Resources/image/obj/title.png");
 	Vec2 titleImageSize{ 625.0f,125.0f };
 	titleBar.Init(titlePos, titleSize, Vec2(0.0f, 0.0f),
 		titleGH, Vec2(0.0f, 0.0f), titleImageSize
@@ -21,8 +21,8 @@ void Scene_Title::Load() {
 	
 	//ボタンサイズは統一する
 	Vec2 buttonSize{ 500.0f,100.0f };
-	const char* goSelectGH = "./Resource/image/obj/button/goSelectButton.png";
-	const char* goSelectClickedGH = "./Resource/image/obj/button/goSelectCickedButton.png";
+	const char* goSelectGH = "./Resources/image/obj/button/goSelectButton.png";
+	const char* goSelectClickedGH = "./Resources/image/obj/button/goSelectCickedButton.png";
 	Button_goSelect.Init(goSelectPos, buttonSize, Vec2(0.0f, 0.0f),
 		goSelectGH, goSelectClickedGH, 
 		Vec2(0.0f, 0.0f), buttonSize);
@@ -31,8 +31,8 @@ void Scene_Title::Load() {
 	Vec2 menuButtonPos{ Button_goSelect.getScreenLbVertex().x,
 		Button_goSelect.getScreenLbVertex().y + 50.0f };
 
-	const char* goMenuGH = "./Resource/image/obj/button/menu.png";
-	const char* goMenuClickedGH = "./Resource/image/obj/button/menuClicked.png";
+	const char* goMenuGH = "./Resources/image/obj/button/menu.png";
+	const char* goMenuClickedGH = "./Resources/image/obj/button/menuClicked.png";
 
 	Button_goManu.Init(menuButtonPos, buttonSize, Vec2(0.0f, 0.0f),
 		goMenuGH, goMenuClickedGH,
@@ -40,8 +40,8 @@ void Scene_Title::Load() {
 
 	Vec2 exitButtonPos{ menuButtonPos.x,menuButtonPos.y + 50.0f };
 	
-	const char* exitGH = "./Resource/image/obj/button/exit.png";
-	const char* exitClickedGH = "./Resource/image/obj/button/exitClicked.png";
+	const char* exitGH = "./Resources/image/obj/button/exit.png";
+	const char* exitClickedGH = "./Resources/image/obj/button/exitClicked.png";
 	
 	Button_exit.Init(exitButtonPos, buttonSize, Vec2(0.0f, 0.0f),
 		exitGH, exitClickedGH, Vec2(0.0f, 0.0f), buttonSize);
