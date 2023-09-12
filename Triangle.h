652c118fe44b0c78549224e42406aef05c402276
tou ama <ira341394@gmail.com>
2023-09-12 21:34:35 +0900

    a

--- Triangle.h
+++ Triangle.h
@@ -1,10 +1,20 @@
 #pragma once
 #include"Vec2.h"
+#include"Coordinate.h"
 
 struct Triangle {
-	Vec2 pos_;
 
-	void Draw() {}
+	Coordinate localCo{ Vec2(0.0f,0.0f) };
+	Vec2 vertex_[3];
+	
+	float currentTime;
+	float t;
+
+	unsigned int color;
+
+	void EasingUpdate();
+
+	void Draw();
 
 
 
