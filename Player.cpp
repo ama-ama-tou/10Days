b521bf6ef86216501ddd1beb5d719b6e0c6a16dc
陽人 加瀬 <0te06559726481t@au.com>
2023-09-13 22:38:21 +0900

    a

--- Player.cpp
+++ Player.cpp
@@ -67,4 +67,8 @@ void Player::Draw() {
 	Quad::Draw();
 
 
+	Novice::ScreenPrintf(1000, 300, "faicing_[0]:%d(top)", getIsFacingTop());
+	Novice::ScreenPrintf(1000, 320, "faicing_[1]:%d(right)", getIsFacingRight());
+	Novice::ScreenPrintf(1000, 340, "faicing_[2]:%d(bottom)", getIsFacingBottom());
+	Novice::ScreenPrintf(1000, 360, "faicing_[3]:%d(left)", getIsFacingLeft());
 }
