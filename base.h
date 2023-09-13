1c117d379900fa608b95e01a0b577491a1e02ec4
tou ama <ira341394@gmail.com>
2023-09-13 12:49:15 +0900

    a

--- base.h
+++ base.h
@@ -6,5 +6,5 @@ const Vec2 kWindowSize(1280.0f, 720.0f);
 const Vec2 kFieldLtPos(320.0f, 40.0f);
 const Vec2 kFieldSize(640.0f, 640.0f);
 
-const int kBeforeColor = 0x53558bff;
-const int kAfterColor= 0xe28f8fff;
+unsigned const int kBeforeColor = 0x53558bff;
+unsigned const int kAfterColor= 0xe28f8fff;
